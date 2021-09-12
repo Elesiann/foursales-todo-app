ae8aaac42daa0ff7488b4e2c6ed8f3b569966c24
Giovani <giovanimcorrea18@gmail.com>
2021-09-12 17:33:30 -0300

    deleting is fixed, handleSubmit function still doesnt reset values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import React, { useState, setState } from "react";
+import React, { useState } from "react";
 
 export function App() {
   const [values, setValues] = useState([
     {
-      id: new Date(),
+      id: Math.floor(Math.random() * 1000),
       descricao: "",
       atividade: "",
       trabalho: true,
@@ -13,6 +13,8 @@ export function App() {
   ]);
 
   const [atividades, setAtividades] = useState([]);
+
+  // const numbers = values.map( o => o.id );
 
   function handleChange(event) {
     setValues({
@@ -24,10 +26,10 @@ export function App() {
 
   function handleSubmit(event) {
     event.preventDefault();
+
     setAtividades([
       ...atividades,
       {
-        id: values.id,
         nome: values.atividade,
         descricao: values.descricao,
         checkTrabalho: values.trabalho,
@@ -40,7 +42,7 @@ export function App() {
     const updatedTodos = [...atividades].filter(
       (atividades) => atividades.id !== id
     );
-    console.log(values.id);
+    console.log(values[0]["id"]);
     setAtividades(updatedTodos);
   }
 
@@ -87,31 +89,31 @@ export function App() {
       {/* ========================================================================================== */}
 
       {/* ========================================================================================== */}
-      <div id="container" key={values.id}>
+      <div id="container">
         <div>
           <h1>Trabalhos</h1>
           {atividades.map(
             (atividade) =>
               atividade.checkTrabalho === true && (
-                <div className="todos">
+                <div key={values.id} className="todos">
                   Todo: {atividade.nome} <br />
                   descricao: {atividade.descricao} <br />
-                  id: {values.id} <br />
+                  id: {values[0]["id"]} <br />
                   <button onClick={() => deleteTodo()}>deletar</button>
                 </div>
               )
           )}
         </div>
 
-        <div key={values.id}>
+        <div>
           <h1>Pessoal</h1>
           {atividades.map(
             (atividade) =>
               atividade.checkPessoal === true && (
-                <div className="todos">
+                <div key={values.id} className="todos">
                   Todo: {atividade.nome} <br />
                   descricao: {atividade.descricao} <br />
-                  id: {values.id} <br />
+                  id: {values[0]["id"]} <br />
                   <button onClick={() => deleteTodo()}>deletar</button>
                 </div>
               )
