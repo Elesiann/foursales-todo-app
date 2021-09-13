9133a198c7f89f6f6c6faf8922553d9a7d777ef8
Giovani <giovanimcorrea18@gmail.com>
2021-09-13 17:39:34 -0300

    id fixed, using react-uuid, delete working

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, setState } from "react";
+import uuid from "react-uuid";
 
 export function App() {
   const [values, setValues] = useState([
     {
-      id: Math.floor(Math.random() * 1000),
       descricao: "",
       atividade: "",
       trabalho: true,
-      pessoal: false,
-    },
+      pessoal: false
+    }
   ]);
 
   const [atividades, setAtividades] = useState([]);
-
-  // const numbers = values.map( o => o.id );
 
   function handleChange(event) {
     setValues({
       ...values,
       [event.target.name]: event.target.value,
-      [event.target.descricao]: event.target.descricao,
+      [event.target.descricao]: event.target.descricao
     });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-
     setAtividades([
       ...atividades,
       {
+        id: uuid(),
         nome: values.atividade,
         descricao: values.descricao,
         checkTrabalho: values.trabalho,
-        checkPessoal: values.pessoal,
-      },
+        checkPessoal: values.pessoal
+      }
     ]);
   }
 
   function deleteTodo(id) {
-    const updatedTodos = [...atividades].filter(
-      (atividades) => atividades.id !== id
-    );
-    console.log(values[0]["id"]);
+    const updatedTodos = atividades.filter((atividade) => atividade.id !== id);
+
     setAtividades(updatedTodos);
   }
 
@@ -95,11 +91,13 @@ export function App() {
           {atividades.map(
             (atividade) =>
               atividade.checkTrabalho === true && (
-                <div key={values.id} className="todos">
+                <div className="todos" key={atividade.id}>
                   Todo: {atividade.nome} <br />
                   descricao: {atividade.descricao} <br />
-                  id: {values[0]["id"]} <br />
-                  <button onClick={() => deleteTodo()}>deletar</button>
+                  id: {atividade.id} <br />
+                  <button onClick={() => deleteTodo(atividade.id)}>
+                    deletar
+                  </button>
                 </div>
               )
           )}
@@ -110,11 +108,13 @@ export function App() {
           {atividades.map(
             (atividade) =>
               atividade.checkPessoal === true && (
-                <div key={values.id} className="todos">
+                <div className="todos" key={atividade.id}>
                   Todo: {atividade.nome} <br />
                   descricao: {atividade.descricao} <br />
-                  id: {values[0]["id"]} <br />
-                  <button onClick={() => deleteTodo()}>deletar</button>
+                  id: {atividade.id} <br />
+                  <button onClick={() => deleteTodo(atividade.id)}>
+                    deletar
+                  </button>
                 </div>
               )
           )}
